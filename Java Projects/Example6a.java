c7e8c785cabd003dc7ad3b1ad3e454aebc15fad7
Dylan Goertz <dylan.goertz1130@gmail.com>
2025-05-04 14:16:11 -0500

    Update Example6a.java

--- Java Projects/Example6a.java
+++ Java Projects/Example6a.java
@@ -12,13 +12,13 @@ public class Example6a
         Scanner keyboard = new Scanner(System.in);
 
         System.out.print("\nWhat city is the capitol of France? ");
-        keybaord.next(); //alows inout of a word
+        keyboard.next(); //alows inout of a word
 
         System.out.print("what is 6 multiplied by 7? ");
-        keybaord.next(); //alows inout of an integer
+        keyboard.next(); //alows inout of an integer
 
         System.out.print("Enter a number between 0.0 and 1.0: ");
-        keybaord.next(); //alows inout of a fractional value
+        keyboard.next(); //alows inout of a fractional value
 
         System.out.println("\nI forgot what you typed in!");
     }
